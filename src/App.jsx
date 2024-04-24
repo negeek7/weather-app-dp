e6b0bccd954e4bc675b3fb9046581b0114201a2e
Harshiiet Negi <negeek7@gmail.com>
2024-04-25 02:49:46 +0530

    api info

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,9 @@ function App() {
 
   return (
     <>
-      <p className="text-3xl font-bold underline bg-red-900" >Hello</p> 
+    <div className="flex flex-col items-center justify-center h-screen w-screen text-white-700 p-10 bg-gradient-to-r from-slate-900 to-slate-700">
+      <p>Hello</p> 
+    </div>
     </>
   )
 }
